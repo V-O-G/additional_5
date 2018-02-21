00f377f135e58e11cef62432a9a87f4ee609d872
V-O-G <cornflower1989@gmail.com>
2018-02-21 23:48:14 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for(j=0;j<bracketsConfig.length;j++){
+     for(i=0;i<=str.length;){
+     if (str[i]+str[i+1]==bracketsConfig[j].join('')){
+       str=str.replace(str[i]+str[i+1],'');
+    i=0;
+    j=0;
+      continue;
+     }
+     i++;
+  }
+  }
+ if(str.length===0){return true}
+ else {return false}
 }
